d5fa6115ac7ef59833e7ae9f3ddd478bb83cc857
Nedd Chairi <neddyjob@gmail.com>
2023-09-04 16:54:10 +0200

    Function to read files line by line

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use std::{
+    fs::File,
+    io::{BufRead, BufReader},
+};
+
 fn main() {
-    println!("Hello, world!");
+    let variables = "hola.txt";
+    let workflow = "hola.txt";
+
+    read_file_line_by_line(variables);
+    read_file_line_by_line(workflow);
+}
+
+fn read_file_line_by_line(filepath: &str) -> Result<(), Box<dyn std::error::Error>> {
+    let file = File::open(filepath)?;
+    let reader = BufReader::new(file);
+
+    for line in reader.lines() {
+        println!("{}", line?);
+    }
+
+    Ok(())
 }
