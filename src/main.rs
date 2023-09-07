12f095acdc18faa1e627f6849c3382752a00c542
Nedd Chairi <neddyjob@gmail.com>
2023-09-07 10:55:28 +0200

    Todo bien, falta validación

--- src/main.rs
+++ src/main.rs
@@ -3,18 +3,53 @@ use std::fs;
 use std::io::Write;
 
 fn main() {
+    // Form
+    let stdin = std::io::stdin();
+
+    // Variables files
     let vars_file: String = String::from("replace.txt");
     let vars = scan_variables(vars_file);
 
-    let new_file = String::from("newworkflow.yaml");
-    let old_file = String::from("workflow.yaml");
-    let new_workflow = create_new_file(old_file, new_file, vars);
+    // File to clone
+    println!("Write the name of the file to process: ");
+    let mut old_file = String::new();
+    match stdin.read_line(&mut old_file) {
+        Ok(read_file) => read_file,
+        Err(e) => {
+            println!("Error reading the prompt: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    // Cloned file
+    println!("Write the new name of the file (Default: export.yaml): ");
+    let mut new_file = String::new();
+
+    match stdin.read_line(&mut new_file) {
+        Ok(read_file) => read_file,
+        Err(e) => {
+            println!("Error reading the prompt: {}", e);
+            std::process::exit(1);
+        }
+    };
+
+    if new_file.is_empty() {
+        new_file = String::from("export.yaml")
+    }
+
+    let new_workflow = create_new_file(&mut old_file, &mut new_file, vars);
     println!("{}", new_workflow);
 }
 
 fn scan_variables(file: String) -> HashMap<String, String> {
     let mut vars: HashMap<String, String> = HashMap::new();
-    let binding = fs::read_to_string(file).unwrap();
+    let binding = match fs::read_to_string(file) {
+        Ok(bind) => bind,
+        Err(e) => {
+            println!("Error binding the variables: {}", e);
+            std::process::exit(1);
+        }
+    };
 
     for line in binding.lines() {
         let temp_line: Vec<&str> = line.split(':').collect();
@@ -26,8 +61,14 @@ fn scan_variables(file: String) -> HashMap<String, String> {
     vars
 }
 
-fn create_new_file(old_file: String, new_file: String, vars: HashMap<String, String>) -> String {
-    let old_data = fs::read_to_string(old_file).unwrap();
+fn create_new_file(old_file: &String, new_file: &String, vars: HashMap<String, String>) -> String {
+    let old_data = match fs::read_to_string(&old_file.trim()) {
+        Ok(data) => data,
+        Err(e) => {
+            println!("Error: {}", e);
+            std::process::exit(1);
+        }
+    };
 
     let mut new_data = old_data.clone();
 
@@ -37,8 +78,20 @@ fn create_new_file(old_file: String, new_file: String, vars: HashMap<String, Str
         }
     }
 
-    let mut end_file = fs::File::create(new_file).unwrap();
-    end_file.write(new_data.as_bytes()).unwrap();
+    let mut end_file = match fs::File::create(new_file.trim()) {
+        Ok(file) => file,
+        Err(e) => {
+            println!("Error: {}", e);
+            std::process::exit(1);
+        }
+    };
+    match end_file.write(new_data.as_bytes()) {
+        Ok(file) => file,
+        Err(e) => {
+            println!("Error: {}", e);
+            std::process::exit(1);
+        }
+    };
 
     let info = String::from("File exported correctly");
     info
